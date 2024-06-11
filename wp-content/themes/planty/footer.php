9e8ba61bc38958b846291a6c440013c4649cc8a6
Lucie Albane <curti.lucie@protonmail.com>
2024-06-11 14:16:05 +0200

    header et footer modifiés pour coller avec les maquettes

---
+++ wp-content/themes/planty/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+			<footer id="site-footer" class="header-footer-group">
+
+				<div class="section-inner">
+
+					<div class="footer-credits">
+
+						<?php
+						echo '<p class="privacy-policy"> <a href="#"> Mentions légales </a> </p>';
+						?>
+
+					</div><!-- .footer-credits -->
+
+				</div><!-- .section-inner -->
+
+			</footer><!-- #site-footer -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
